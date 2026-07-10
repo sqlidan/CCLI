5f5237a0fcf0f8a321f26bbc973d3e78ec3b4276
13482441774 <chi166897.>
2026-07-10 09:16:44 +0800

    1.取消判断未上架的费用生成

--- src/main/java/com/haiersoft/ccli/cost/web/StandingBookController.java
+++ src/main/java/com/haiersoft/ccli/cost/web/StandingBookController.java
@@ -738,9 +738,9 @@ public class StandingBookController extends BaseController {
     public String saveCwCost(HttpServletRequest request) throws Exception{
     	String linkId=request.getParameter("linkId");
     	String feeId=request.getParameter("feeId");
-    	if(!trayInfoService.hasAllUpShelfTray(linkId)){
-    		return "当前联系单存在未上架库存，不能进行费用操作！";
-    	}
+//    	if(!trayInfoService.hasAllUpShelfTray(linkId)){
+//    		return "当前联系单存在未上架库存，不能进行费用操作！";
+//    	}
     	BisEnterStock obj = enterStockService.get(linkId);//入库联系单id
     	List<String> feeIds=new ArrayList<String>();
         feeIds.add(feeId);
